efa5ebd09b13d6dac08dc7da2f96cf6dc0c8073c
Bailey Cage <bailey.cage@gmail.com>
2022-08-09 09:59:19 -0400

    add anon func to const returnLastTwoDrivers

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 const returnFirstTwoDrivers = (driverAry) => [driverAry[0], driverAry[1]];
+
+const returnLastTwoDrivers = (driverAry) => {
+    let last = driverAry.length - 1;
+    return [driverAry[last-1], driverAry[last]];
+}
