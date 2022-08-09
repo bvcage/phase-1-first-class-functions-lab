986dc67b065a60c818d186511c7b1affcdda3bc6
Bailey Cage <bailey.cage@gmail.com>
2022-08-09 11:05:23 -0400

    create func in const fareDoubler

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const returnFirstTwoDrivers = (driverAry) => [driverAry[0], driverAry[1]];
 const returnLastTwoDrivers = (driverAry) => {
     let last = driverAry.length - 1;
     return [driverAry[last-1], driverAry[last]];
-}
+};
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
-function createFareMultiplier (multiplier) {
+const createFareMultiplier = (multiplier) => {
     return (fare) => multiplier * fare;
-}
+};
+
+const fareDoubler = (fare) => createFareMultiplier(2)(fare);
